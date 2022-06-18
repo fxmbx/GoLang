934a7a484669c931f88ff76a8e7850b5801097f8
fxmbx <funbiolaore@gmail.com>
2022-06-18 20:51:03 +0100

    setting up server and routes

---
+++ go-stripe/cmd/web/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+ 
+const version = "1.0.0"
+
+const cssVersion = "1"
+
+type config  struct {
+	port int 
+	env string
+	api string 
+	db struct {
+		dsn string
+	}
+	stripe struct {
+		secret string
+		key string 
+	}
+}
+
+type application struct {
+	config config 
+	infoLog *log.Logger
+	errorLog *log.Logger
+	templateCache map[string]*template.Template
+	version string 
+}
+
+
+func (app *application) serve () error {
+	srv := &http.Server {
+		Addr: fmt.Sprintf(":%d", app.config.port),
+		Handler: app.routes(),
+		IdleTimeout: 30 * time.Second,
+		ReadTimeout: 10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	app.infoLog.Printf("Starting http server in %v on port %v ",app.config.env, app.config.port)
+
+	return srv.ListenAndServe()
+}
+
+func main(){
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port",4000," server port to listen on")
+	flag.StringVar(&cfg.env, "env","development","application environment {development|production}")
+	flag.StringVar(&cfg.api, "api","http://localhost:4001","Url to api")
+
+	flag.Parse()
+
+	cfg.stripe.secret = os.Getenv("STRIPE_SECRET")
+	cfg.stripe.key = os.Getenv("STRIPE_KEY")
+
+	infoLog := log.New(os.Stdout, "INFO\t",log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t",log.Ldate|log.Ltime|log.Lshortfile)
+
+
+	tc := make(map[string]*template.Template)
+
+	app :=  &application{
+		infoLog: infoLog,
+		errorLog: errorLog,
+		version: version,
+		templateCache: tc,
+		config: cfg,
+	}
+
+	err:=app.serve()
+	if err != nil {
+		app.errorLog.Println(err)
+		log.Fatal(err)
+		
+	}
+}
